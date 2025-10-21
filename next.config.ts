ab37c9ab7457374d2e9c0adef67788492b366419
Dawid Glapiak <dg18@live.it>
2025-10-21 11:00:55 +0200

    ignore eslint

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
+// next.config.ts
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    // Non bloccare il build su errori/warning ESLint
+    ignoreDuringBuilds: true,
+  },
+  // Se vuoi (solo se serve) ignorare anche errori TypeScript a build:
+  // typescript: { ignoreBuildErrors: true },
 };
 
 export default nextConfig;
