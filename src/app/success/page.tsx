9219d58865f5a25988aef78e7971a882f4522102
Dawid Glapiak <dg18@live.it>
2025-10-22 21:48:30 +0200

    fix again success

--- src/app/success/page.tsx
+++ src/app/success/page.tsx
@@ -1,13 +1,14 @@
 // src/app/checkout/success/page.tsx
+// ⬅️ NON "../checkout/success/page"
 
-import SuccessClient from "../checkout/success/page";
-
+import SuccessClient from "./SuccessClient";
 
 export default function Page({
   searchParams,
 }: {
   searchParams: { [key: string]: string | string[] | undefined };
 }) {
-  const sessionId = typeof searchParams?.session_id === 'string' ? searchParams.session_id : '';
+  const sessionId =
+    typeof searchParams?.session_id === 'string' ? searchParams.session_id : '';
   return <SuccessClient sessionId={sessionId} />;
 }
